f4cb6bad3b447751890a34cc700e30a9a57e875a
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-09-29 13:46:00 +0800

    chore: remove base path

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,8 +6,6 @@ const createOptions = (req) => ({
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      redirectUri:
-        "https://next-auth-test-rewrites.vercel.app/dev-days-test/api/auth/callback/github",
     }),
   ],
 });
